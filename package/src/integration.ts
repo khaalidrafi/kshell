d2598fc2184e84bd6567d0f2a68a207f239c55ad
Louis Escher <louisescher@proton.me>
2026-01-01 20:13:17 +0100

    chore: Upgrade dependencies, fix lint issues

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -1,141 +1,147 @@
 import type { AstroIntegration } from 'astro';
+import { z } from 'astro/zod';
 import { viteVirtualModulePluginBuilder } from './utils/virtual-module-plugin-builder';
-import { z } from 'astro/zod'; 
 
 const openGraphOptionsSchema = z.object({
-  /**
-   * The title of the page.
-   */
-  title: z.string(),
-  /**
-   * The description of the page. Optional.
-   */
-  description: z.string().optional(),
+	/**
+	 * The title of the page.
+	 */
+	title: z.string(),
+	/**
+	 * The description of the page. Optional.
+	 */
+	description: z.string().optional(),
 });
 
-const giscusObjectSchema = z.object({
-  /**
-   * The repository name.
-   */
-  repository: z.string(),
-  /**
-   * The repository's ID.
-   */
-  repositoryId: z.string(),
-  /**
-   * The category of the repository.
-   */
-  category: z.string(),
-  /**
-   * The category's ID.
-   */
-  categoryId: z.string(),
-  /**
-   * The mapping of the comments.
-   */
-  mapping: z.union([
-    z.literal('pathname'),
-    z.literal('url'),
-    z.literal('title'),
-    z.literal('og:title'),
-    z.literal('specific'),
-    z.literal('number')
-  ]),
-  /**
-   * The term to use for the comments.
-   */
-  term: z.string().optional(),
-  /**
-   * Whether the comments are strict.
-   */
-  strict: z.boolean(),
-  /**
-   * Whether reactions are enabled.
-   */
-  reactionsEnabled: z.boolean(),
-  /**
-   * Whether metadata should be emitted.
-   */
-  emitMetadata: z.boolean(),
-  /**
-   * The theme to use for the comments. Defaults to `https://spectre.louisescher.dev/styles/giscus`.
-   */
-  theme: z.string().optional(),
-  /**
-   * The language to use for the comments.
-   */
-  lang: z.string(),
-  /**
-   * Where the comments input should be placed. Default is `bottom`.
-   */
-  commentsInput: z.union([
-    z.literal('bottom'),
-    z.literal('top'),
-  ]).optional(),
-}).refine((data) => {
-  if (data.mapping === 'specific' || data.mapping === 'number') {
-    return !!data.term;
-  }
+const giscusMappingSchema = z.union([
+	z.literal('pathname'),
+	z.literal('url'),
+	z.literal('title'),
+	z.literal('og:title'),
+	z.literal('specific'),
+	z.literal('number'),
+]);
 
-  return true;
-}).optional();
+const giscusObjectSchema = z
+	.object({
+		/**
+		 * The repository name.
+		 */
+		repository: z.string(),
+		/**
+		 * The repository's ID.
+		 */
+		repositoryId: z.string(),
+		/**
+		 * The category of the repository.
+		 */
+		category: z.string(),
+		/**
+		 * The category's ID.
+		 */
+		categoryId: z.string(),
+		/**
+		 * The mapping of the comments.
+		 */
+		mapping: giscusMappingSchema,
+		/**
+		 * The term to use for the comments.
+		 */
+		term: z.string().optional(),
+		/**
+		 * Whether the comments are strict.
+		 */
+		strict: z.boolean(),
+		/**
+		 * Whether reactions are enabled.
+		 */
+		reactionsEnabled: z.boolean(),
+		/**
+		 * Whether metadata should be emitted.
+		 */
+		emitMetadata: z.boolean(),
+		/**
+		 * The theme to use for the comments. Defaults to `https://spectre.louisescher.dev/styles/giscus`.
+		 */
+		theme: z.string().optional(),
+		/**
+		 * The language to use for the comments.
+		 */
+		lang: z.string(),
+		/**
+		 * Where the comments input should be placed. Default is `bottom`.
+		 */
+		commentsInput: z.union([z.literal('bottom'), z.literal('top')]).optional(),
+	})
+	.refine((data) => {
+		if (data.mapping === 'specific' || data.mapping === 'number') {
+			return !!data.term;
+		}
+
+		return true;
+	})
+	.optional();
 
 export const optionsSchema = z.object({
-  /**
-   * The name that should be displayed on the main page.
-   */
-  name: z.string(),
-  /**
-   * The theme color of the site. Optional. Defaults to `#8c5cf5`.
-   */
-  themeColor: z.string().optional(),
-  /**
-   * The Twitter handle of the site. Used for Twitter meta tags. Optional.
-   */
-  twitterHandle: z.string().optional(),
-  /**
-   * Open Graph meta tags for various pages.
-   */
-  openGraph: z.object({
-    /**
-     * Open Graph meta tags for the home page.
-     */
-    home: openGraphOptionsSchema,
-    /**
-     * Open Graph meta tags for the blog page.
-     */
-    blog: openGraphOptionsSchema,
-    /**
-     * Open Graph meta tags for the projects page.
-     */
-    projects: openGraphOptionsSchema,
-  }),
-  /**
-   * All of this information can be find on [giscus' config page](https://giscus.app) under "Enable giscus" after entering all information.
-   */
-  giscus: z.union([
-    z.literal(false),
-    giscusObjectSchema
-  ])
+	/**
+	 * The name that should be displayed on the main page.
+	 */
+	name: z.string(),
+	/**
+	 * The theme color of the site. Optional. Defaults to `#8c5cf5`.
+	 */
+	themeColor: z.string().optional(),
+	/**
+	 * The Twitter handle of the site. Used for Twitter meta tags. Optional.
+	 */
+	twitterHandle: z.string().optional(),
+	/**
+	 * Open Graph meta tags for various pages.
+	 */
+	openGraph: z.object({
+		/**
+		 * Open Graph meta tags for the home page.
+		 */
+		home: openGraphOptionsSchema,
+		/**
+		 * Open Graph meta tags for the blog page.
+		 */
+		blog: openGraphOptionsSchema,
+		/**
+		 * Open Graph meta tags for the projects page.
+		 */
+		projects: openGraphOptionsSchema,
+	}),
+	/**
+	 * All of this information can be find on [giscus' config page](https://giscus.app) under "Enable giscus" after entering all information.
+	 */
+	giscus: z.union([z.literal(false), giscusObjectSchema]),
 });
 
+export type GiscusMapping = z.infer<typeof giscusMappingSchema>;
+
 export default function integration(options: z.infer<typeof optionsSchema>): AstroIntegration {
-  if (typeof options.giscus === "object") {
-    const giscusOpts = (options.giscus as z.infer<typeof giscusObjectSchema>)!;
-    const likelyUntouchedConfig = Object.keys(giscusOpts).every((key) => {
-      const item = giscusOpts[key as keyof typeof giscusOpts];
+	if (typeof options.giscus === 'object') {
+		const giscusOpts = (options.giscus as z.infer<typeof giscusObjectSchema>)!;
+		const likelyUntouchedConfig = Object.keys(giscusOpts).every((key) => {
+			const item = giscusOpts[key as keyof typeof giscusOpts];
 
-      return typeof item === "undefined" || item === false;
-    });
+			return typeof item === 'undefined' || item === false;
+		});
 
-    if (likelyUntouchedConfig) {
-      throw new Error("\n\nERROR: It seems you have not updated the preset Giscus configuration for comments! Please change the settings in your astro.config.mjs by adding a .env with the required variables, adding the strings right in your configuration or removing the `giscus` option altogether.\n\n");
-    }
-  }
+		if (likelyUntouchedConfig) {
+			throw new Error(
+				'\n\nERROR: It seems you have not updated the preset Giscus configuration for comments! Please change the settings in your astro.config.mjs by adding a .env with the required variables, adding the strings right in your configuration or removing the `giscus` option altogether.\n\n'
+			);
+		}
+	}
 
-  const validatedOptions = optionsSchema.parse(options);
+	const validatedOptions = optionsSchema.parse(options);
 
-	const globals = viteVirtualModulePluginBuilder('spectre:globals', 'spectre-theme-globals', `
+	const globals = viteVirtualModulePluginBuilder(
+		'spectre:globals',
+		'spectre-theme-globals',
+		`
     export const name = ${JSON.stringify(validatedOptions.name)};
     export const themeColor = ${JSON.stringify(validatedOptions.themeColor ?? '#8c5cf5')};
     export const twitterHandle = ${JSON.stringify(validatedOptions.twitterHandle)};
@@ -145,7 +151,8 @@ export default function integration(options: z.infer<typeof optionsSchema>): Ast
       projects: ${JSON.stringify(validatedOptions.openGraph.projects)},
     };
     export const giscus = ${validatedOptions.giscus ? JSON.stringify(validatedOptions.giscus) : 'false'};
-  `);
+  `
+	);
 
 	return {
 		name: 'spectre-theme',
@@ -153,9 +160,7 @@ export default function integration(options: z.infer<typeof optionsSchema>): Ast
 			'astro:config:setup': ({ updateConfig }) => {
 				updateConfig({
 					vite: {
-						plugins: [
-              globals(),
-            ],
+						plugins: [globals()],
 					},
 				});
 			},
--- src/pages/styles/giscus.ts
+++ src/pages/styles/giscus.ts
@@ -3,11 +3,11 @@ export const prerender = false;
 import css from '../../styles/giscus.css?raw';
 
 export async function ALL() {
-  const res = new Response(css);
+	const res = new Response(css);
 
-  res.headers.set('Content-Type', 'text/css');
-  res.headers.set('Access-Control-Allow-Origin', '*');
-  res.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
+	res.headers.set('Content-Type', 'text/css');
+	res.headers.set('Access-Control-Allow-Origin', '*');
+	res.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
 
-  return res;
+	return res;
 }

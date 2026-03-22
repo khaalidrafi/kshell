cb7dfb9d011e4881e0b5904a01173e24067e31ad
Khalid Rafi <khaalidrafi@gmail.com>
2026-03-23 00:19:58 +0600

    Clean Emacs config with new macro

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,22 +1,11 @@
-((nil . ((eval . (prettier-js-mode 0))
-         (eval . (add-node-modules-path))
+((nil . (
+         (biomejs-format-biome-command . "~/kshell/node_modules/.bin/biome")
          (eval . (biomejs-format-mode 1))
          (web-mode-css-indent-offset . 2)
          (eval .
-               (defun kshell/astro-server ()
-                 (interactive)
-                 (khalid/project-run-async-shell-command "pnpm dev" "*kshell-server*" nil (project-current nil "."))))
+               (project-make-commands
+                "server" "pnpm dev"
+                "deploy" "pnpm run deploy"))
          
-         (khalid/project-commands . (kshell/astro-server))
-
          (khalid/project-local-url . "http://localhost:4321")
-
-         (eval .
-               (defun kshell/deploy ()
-                 (interactive)
-                 (khalid/project-run-async-shell-command "pnpm run deploy" "*kshell-deploy*")))
-
-         (khalid/project-deploy-command . kshell/deploy)
-         
-         (eval .
-               (khalid/set-project-command-keybindings)))))
+         )))
